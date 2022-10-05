a275fc160dd81186620d5d54f3caef4cf4f0f188
kikiki10004 <kikiki10004@gmail.com>
2022-10-05 11:56:48 +1100

    secondwhile

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let's do this");
+var incrementer= 0;
+var stopValue = 20;
+
+//repetition 반복 할거임 코더의 책임은 매인테인
+
+
+while (incrementer <= stopValue) {
+
+    Console.WriteLine(incrementer);
+    
+    incrementer++;
+    if(incrementer>20){
+        Console.WriteLine("end");
+        break;
+    }
+    System.Console.WriteLine(incrementer);
+    
+    incrementer++;
+
+}
+
+
+
